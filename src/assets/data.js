2bc27f27b7af2f474c17c90f252d4379e6798b5e
Kerie <ogedegbekerieo@gmail.com>
2024-05-03 08:54:23 +0100

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    question: "Which one of these roomates is oldest?",
+    option1: "Emmanuel",
+    option2: "Kerie",
+    option3: "David",
+    option4: "Eugene",
+    ans: 4,
+  },
+  {
+    question: "Which one of these is a FootBaller?",
+    option1: "Lebron James",
+    option2: "Serena Williams",
+    option3: "Lionel Messi",
+    option4: "Lewis Hamilton",
+    ans: 3,
+  },
+  {
+    question: "Who is the current president of the US?",
+    option1: "Joe Biden",
+    option2: "Hilary Clinton",
+    option3: "Donald Trump",
+    option4: "Abraham Lincoln",
+    ans: 1,
+  },
+  {
+    question: "Who's the friendliest character?",
+    option1: "Sasuke Uchiha",
+    option2: "Luffy D Monkey",
+    option3: "Levi Ackerman",
+    option4: "Anos Voldigoad",
+    ans: 2,
+  },
+];
