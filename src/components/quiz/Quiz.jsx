6f8cd7f6888c562db74c39dda676caa7c3353f27
Kerie <ogedegbekerieo@gmail.com>
2024-05-23 23:24:30 +0100

    okay

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -15,7 +15,7 @@ const Quiz = () => {
   let Option4 = useRef(null);
 
   let option_array = [Option1, Option2, Option3, Option4];
-
+// check answers function
   const checkAns = (e, ans) => {
     if (lock === false) {
       if (question.ans === ans) {
